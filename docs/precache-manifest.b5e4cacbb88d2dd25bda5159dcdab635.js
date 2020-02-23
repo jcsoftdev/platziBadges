788501413fd3c65b2edc528dbff1cadd5b41bb0f
Juan Carlos <ajcarlos032@gmail.com>
2020-02-23 17:49:47 -0500

    modify

---
+++ docs/precache-manifest.b5e4cacbb88d2dd25bda5159dcdab635.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a0f4848baf857429d3",
+    "url": "/static/css/main.3b23e3e7.chunk.css"
+  },
+  {
+    "revision": "17a0f4848baf857429d3",
+    "url": "/static/js/main.17a0f484.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e9d06fdd15282ca1edad",
+    "url": "/static/css/2.46f5d107.chunk.css"
+  },
+  {
+    "revision": "e9d06fdd15282ca1edad",
+    "url": "/static/js/2.e9d06fdd.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "40139607b2798ce65b535f868cecfe6a",
+    "url": "/index.html"
+  }
+];
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = `http://${window.location.hostname}:${parseInt(window.location.port)+1}`;
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
